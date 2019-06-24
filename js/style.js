f1d705449919dc24b3073880da48e35bcb6cb3ab
thacischott <47260946+thacischott@users.noreply.github.com>
2019-06-24 13:20:12 -0300

    Update style.js

--- js/style.js
+++ js/style.js
@@ -1,3 +1,7 @@
+https://code.jquery.com/jquery-1.11.0.min.js
+https://code.jquery.com/jquery-migrate-1.2.1.min.js
+https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick.js
+
 $('.carousel').slick({
   dots: true,
   infinite: true,
