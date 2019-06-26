49a35e76e9531f70c5e8b80747d11ec664225977
thacischott <47260946+thacischott@users.noreply.github.com>
2019-06-26 13:15:32 -0300

    Update style.js

--- js/style.js
+++ js/style.js
@@ -1,5 +1,20 @@
 
 
+function enviar()
+  {
+
+    var nome = document.getElementById("nomeid");
+    console.log(nome);
+    if(nome.value !="")
+    {
+
+      alert('Obrigada '+ nome.value + ', mensagem enviada com sucesso!');
+
+      }
+
+  }
+
+
 $('.carousel').slick({
   dots: true,
   infinite: true,
